3fcdd6264c4f589cbbd6afce4749e1d1986f593f
Audy Huang <od710122@gmail.com>
2017-02-18 07:35:37 +0800

    Implement DFS and BFS

--- Graph.java
+++ Graph.java
@@ -1,39 +1,84 @@
 import java.util.*;
 
 class GraphAlgorithm {
-	private LinkedList<Integer> adj[];
-	private int V;
+	private LinkedList<Integer> mAdjacencyList[];
+	private int mVertexNum;
 
-	public GraphAlgorithm(int v) {
-		V = v;
-		adj = new LinkedList[v];
-		for (int i = 0; i < adj.length; i++) {
-			adj[i] = new LinkedList<Integer>();
+	public GraphAlgorithm(int num) {
+		mVertexNum = num;
+		mAdjacencyList = new LinkedList[num];
+		for (int i = 0; i < mAdjacencyList.length; i++) {
+			mAdjacencyList[i] = new LinkedList<Integer>();
 		}
 	}
 
 	public void addEdge(int v, int w)
 	{
-		adj[v].add(w);
-	}
-
-	private void BFS(int v, boolean visited[])
-	{
+		mAdjacencyList[v].add(w);
 	}
 
 	public void BFS(int s) {
-	}
+		boolean visited[] = new boolean[mVertexNum];
+		ArrayDeque<Integer> explored = new ArrayDeque<Integer>();
+		for (int i = 0; i < visited.length; i++) visited[i] = false;
 
-	private void DFS(int v, boolean visited[])
-	{
+		explored.add(s);
+		visited[s] = true;
+		System.out.println(s);
+
+		while (!explored.isEmpty()) {
+			int v = explored.remove();
+			ListIterator<Integer> iter = mAdjacencyList[v].listIterator();
+			while(iter.hasNext()) {
+				int w = iter.next();
+				if (!visited[w]) {
+					visited[w] = true;
+					System.out.println(w);
+					explored.add(w);
+				}
+			}
+		}
 	}
 
 	public void DFS(int s) {
+		boolean visited[] = new boolean[mVertexNum];
+		Stack<Integer> explored = new Stack<Integer>();
+		for (int i = 0; i < visited.length; i++) visited[i] = false;
+
+		explored.push(s);
+		visited[s] = true;
+		System.out.println(s);
+
+		while (!explored.isEmpty()) {
+			int v = explored.pop();
+			ListIterator<Integer> iter = mAdjacencyList[v].listIterator();
+			while(iter.hasNext()) {
+				int w = iter.next();
+				if (!visited[w]) {
+					visited[w] = true;
+					System.out.println(w);
+					explored.push(w);
+				}
+			}
+		}
 	}
 }
 
 
 public class Graph {
 	public static void main(String[] argv) {
+		GraphAlgorithm ga = new GraphAlgorithm(10);
+		ga.addEdge(0, 4);
+		ga.addEdge(4, 0);
+		ga.addEdge(0, 3);
+		ga.addEdge(3, 0);
+		ga.addEdge(3, 2);
+		ga.addEdge(2, 3);
+		ga.addEdge(2, 1);
+		ga.addEdge(1, 2);
+		ga.addEdge(1, 4);
+		ga.addEdge(4, 1);
+		ga.BFS(4);
+		ga.DFS(4);
 	}
 }
